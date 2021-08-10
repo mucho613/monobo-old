662f5bb7f593c6bdde8dda068d60602236898958
Daisuke Yamamura <mucho613@gmail.com>
2021-08-10 19:46:12 +0900

    add monobo logo(beta)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,13 @@ import DrawingChat from './components/DrawingChat';
 import {
   RecoilRoot
 } from 'recoil';
-
+import logo from './img/logo.svg';
 
 function App() {
   return (
     <RecoilRoot>
       <div className="App">
+      <img src={logo} className="monobo-logo" alt="monobo Drawing chat system" />
         <DrawingChat></DrawingChat>
       </div>
     </RecoilRoot>
